dd59654d666fb676a0179dfaabf9402da2410633
Nicole von Däniken <91322087+nicivd@users.noreply.github.com>
2023-03-14 14:34:18 +0100

    implemented connection to REST API

--- src/app/connection/connection.component.ts
+++ src/app/connection/connection.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PxConnectionSettingsService, PxConnectionSettings, PxHash, PxHttpService } from '@proffix/restapi-angular-library';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-connection',
@@ -7,9 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConnectionComponent implements OnInit {
 
-  constructor() { }
+  restapiForm!: FormGroup;
+  //restApiFormSubmitted = false;
+
+  constructor(
+    private connectionSettingsService: PxConnectionSettingsService,
+    private httpService: PxHttpService,
+    private formBuilder: FormBuilder,
+  ) { }
 
   ngOnInit(): void {
+    this.restapiForm = this.formBuilder.group({
+      restapiurl: ['', Validators.required],
+      restapipassword: ['', Validators.required]
+    })
   }
 
+  public connectToRESTAPI(): void {
+    let connectionSettings: PxConnectionSettings = {
+      WebserviceUrl: this.restapiForm.value.restapiurl,
+      WebservicePasswortHash: PxHash.sha256(this.restapiForm.value.restapipassword)
+    };
+    this.connectionSettingsService.save(connectionSettings);
+    this.isConnectionValid();
+
+  }
+
+  public isConnectionValid() {
+    console.log(this.httpService.get('/PRO/info'));
+  }
+
+  // public getDatabases(): Observable<PxDatenbank[]> {
+  //   return this.pxdatabaseService.getAll();
+  // }
+
+  // public loadDatabases(): void {
+  //   this.getDatabases().subscribe({
+  //     next: (db) => {
+  //       console.log(db);
+  //     }
+  //   })
+  // }
 }
