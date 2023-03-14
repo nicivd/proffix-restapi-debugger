cf2d43aa029c4aa39c9af1e30cb74366fbee9bd5
Nicole von Däniken <91322087+nicivd@users.noreply.github.com>
2023-03-14 10:41:34 +0100

    generated all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ConnectionComponent } from './connection/connection.component';
+import { LoginComponent } from './login/login.component';
+import { DebuggerComponent } from './debugger/debugger.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConnectionComponent,
+    LoginComponent,
+    DebuggerComponent
   ],
   imports: [
     BrowserModule,
