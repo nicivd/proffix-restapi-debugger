3b3f69050febc3e87232f49d37517bd8dc1e4fac
Nicole von Däniken <91322087+nicivd@users.noreply.github.com>
2023-03-14 10:56:51 +0100

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConnectionComponent } from './connection/connection.component';
+import { DebuggerComponent } from './debugger/debugger.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: ConnectionComponent
+  },
+  {
+    path: 'connection', component: ConnectionComponent
+  },
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'debugger', component: DebuggerComponent
+  },
+  {
+    path: '**', component: ConnectionComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
